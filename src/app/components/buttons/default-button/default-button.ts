6b614e3572dd814995755b5ef0408bd506997977
murillotlopes <murillo.tlopes@hotmail.com>
2026-01-22 14:02:01 -0300

    feat: cria botão de propósito geral

---
+++ src/app/components/buttons/default-button/default-button.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-default-button',
+  imports: [],
+  templateUrl: './default-button.html',
+  styleUrl: './default-button.scss',
+})
+export class DefaultButton {
+  @Input() disabled: boolean = false
+  @Output() externalFunction = new EventEmitter<void>();
+
+  externalFunctionActuator() {
+    this.externalFunction.emit()
+  }
+}
