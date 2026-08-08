f88f6b33ac488305da14d1ff7b4c336ec4bb12ee
Murillo Torres Lopes <murillo.tlopes@hotmail.com>
2026-08-08 14:02:36 -0300

    feat: unificação de estilos em styles.scss

--- src/app/pages/private/dashboard/dashboard.ts
+++ src/app/pages/private/dashboard/dashboard.ts
@@ -6,16 +6,26 @@ import { BackButton } from '../../../ui/buttons/back-button/back-button';
 import { ClearButton } from '../../../ui/buttons/clear-button/clear-button';
 import { DefaultButton } from '../../../ui/buttons/default-button/default-button';
 import { SaveButton } from '../../../ui/buttons/save-button/save-button';
+import { FormCheckbox } from '../../../ui/forms/form-checkbox/form-checkbox';
 import { FormGroupComponent } from '../../../ui/forms/form-group/form-group';
+import { FormSelect } from '../../../ui/forms/form-select/form-select';
+import { FormTextArea } from '../../../ui/forms/form-text-area/form-text-area';
+import { FormInputFileList } from '../../../ui/forms/inputs/form-input-file-list/form-input-file-list';
+import { FormInputFile } from '../../../ui/forms/inputs/form-input-file/form-input-file';
 import { FormInputText } from '../../../ui/forms/inputs/form-input-text/form-input-text';
+import { FormInputToggleSwitch } from '../../../ui/forms/inputs/form-input-toggle-switch/form-input-toggle-switch';
 import { FormRadioButton } from '../../../ui/forms/radios/form-radio-button/form-radio-button';
+import { FormRadio } from '../../../ui/forms/radios/form-radio/form-radio';
+import { Pagination } from '../../../ui/navigation/pagination/pagination';
 import { TabPane } from '../../../ui/navigation/tabs-nav/tab-pane/tab-pane';
 import { TabsNav } from '../../../ui/navigation/tabs-nav/tabs-nav';
+import { SkeletonForm } from '../../../ui/skeleton/skeleton-form/skeleton-form';
 import { FormResource } from '../../shared/form-resource';
+import { SkeletonList } from '../skeleton/skeleton-list/skeleton-list';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TabsNav, TabPane, ReactiveFormsModule, FormInputText, SaveButton, BackButton, ClearButton, DefaultButton, FormRadioButton, FormGroupComponent],
+  imports: [TabsNav, TabPane, ReactiveFormsModule, FormInputText, SaveButton, BackButton, ClearButton, DefaultButton, FormRadioButton, FormGroupComponent, FormSelect, FormInputToggleSwitch, FormRadio, FormCheckbox, FormTextArea, FormInputFileList, FormInputFile, SkeletonForm, SkeletonList, Pagination],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
@@ -35,6 +45,15 @@ export class Dashboard extends FormResource {
       who_is_your_self: [null, [RxwebValidators.required({ message: 'Campo obrigatório' }), RxwebValidators.email({ message: 'E-mail inválido' })]],
       who_is_you: [null, RxwebValidators.required({ message: 'Campo obrigatório' })],
       raradio: [null, RxwebValidators.required({ message: 'Campo obrigatório' })],
+      favorite_option: [null, RxwebValidators.required({ message: 'Campo obrigatório' })],
+      toggle: [false],
+      toggle_b: [true],
+      toggle_c: [null],
+      radio: [null],
+      checkbox: [null],
+      textArea: [null],
+      fileList: [null],
+      documents: [null]
     })
   }
   protected override beforeSubmitForm(): void { }
